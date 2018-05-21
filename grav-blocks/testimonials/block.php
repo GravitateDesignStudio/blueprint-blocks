f3680829ea6928e190238c2495a19642f2c2a3b5
Scott Haselwood <scott@Scott-Haselwood.local>
2018-05-21 16:03:11 -0700

    Started on Testimonials Filter

--- grav-blocks/testimonials/block.php
+++ grav-blocks/testimonials/block.php
@@ -1,5 +1,8 @@
 <?php
-if(get_sub_field('testimonials'))
+
+$testimonials = isset($testimonials) ? $testimonials : get_sub_field('testimonials');
+
+if($testimonials)
 {
 	?>
 	<div class="block-inner">
@@ -8,36 +11,56 @@ if(get_sub_field('testimonials'))
 				<div class="swiper-container">
 					<div class="swiper-wrapper">
 
-					<?php
-					while(has_sub_field('testimonials'))
-					{
-						$image = get_sub_field('image');
-						?>
-						<div class="swiper-slide">
-							<div class="<?php echo GRAV_BLOCKS::css()->add(($image ? 'has-image' : 'no-image'))->row()->get();?>">
-								<?php if($image){?>
-								<div class="<?php echo GRAV_BLOCKS::css()->add('col-image')->col(12, 2)->get();?>">
-									<img src="<?php echo esc_attr($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
-								</div>
-								<?php } ?>
-								<div class="<?php echo GRAV_BLOCKS::css()->add('col-content')->col(12, ($image ? 10 : 12))->get();?>">
-									<blockquote class="testimonial">
-										<p><?php the_sub_field('testimonial');?></p>
-										<footer>
-											<cite class="attribution-title"><?php the_sub_field('attribution');?></cite>
-											<?php if($attribution_sub_title = get_sub_field('attribution_sub_title')){ ?>
-												<cite class="attribution-sub-title"><?php echo $attribution_sub_title;?></cite>
-											<?php } ?>
-										</footer>
-									</blockquote>
+						<?php
+						foreach ($testimonials as $testimonial) {
+
+							$image = $testimonial['image'];
+							$text = $testimonial['testimonial'];
+							$attribution = $testimonial['attribution'];
+							$attribution_sub_title = $testimonial['attribution_sub_title'];
+
+							if ($testimonial['use_post']) {
+								echo $testimonial['testimonial_post'];
+								$testimonial_post = get_post($testimonial['testimonial_post']);
+
+								$image = $testimonial['image'];
+								$text = $testimonial_post->post_content;
+								$attribution = $testimonial_post->post_title;
+								$attribution_sub_title = 'CPT';
+								// $text = $testimonial['testimonial'];
+								// $attribution = $testimonial['attribution'];
+								// $attribution_sub_title = $testimonial['attribution_sub_title'];
+
+							} ?>
+
+							<div class="block-testimonials__testimonial swiper-slide">
+								<div class="<?php echo GRAV_BLOCKS::css()->add(($image ? 'has-image' : 'no-image'))->row()->get();?>">
+									<?php if($image){?>
+									<div class="<?php echo GRAV_BLOCKS::css()->add('block-testimonials__testimonial--image')->col(12, 2)->get();?>">
+										<img src="<?php echo esc_attr($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+									</div>
+									<?php } ?>
+									<div class="<?php echo GRAV_BLOCKS::css()->add('block-testimonials__testimonial--content')->col(12, ($image ? 10 : 12))->get();?>">
+										<?php if ($testimonial_post) {
+											var_dump($testimonial_post);
+										} ?>
+										<blockquote class="testimonial">
+											<p><?php echo $text; ?></p>
+											<footer>
+												<cite class="block-testimonials__testimonial--attribution-title"><?php echo $attribution; ?></cite>
+												<?php if($attribution_sub_title){ ?>
+													<cite class="block-testimonials__testimonial--attribution-sub-title"><?php echo $attribution_sub_title;?></cite>
+												<?php } ?>
+											</footer>
+										</blockquote>
+									</div>
 								</div>
 							</div>
-						</div>
-						<?php
-					} ?>
+						<?php } ?>
 
 					</div>
-					<?php if(count(get_sub_field('testimonials')) > 1)
+
+					<?php if(count($testimonials) > 1)
 					{
 						?>
 						<div class="swiper-pagination"></div>
@@ -45,6 +68,7 @@ if(get_sub_field('testimonials'))
     					<div class="swiper-button-next"></div>
 						<?php
 					} ?>
+
 				 </div>
 			 </div>
 		</div>
--- grav-blocks/testimonials/block_fields.php
+++ grav-blocks/testimonials/block_fields.php
@@ -12,6 +12,128 @@
 *
 */
 
+$testimonial_cpts = apply_filters( 'grav_blocks_testimonials_cpt', $testimonial_cpts);
+
+$block_sub_fields = array (
+	array (
+		'key' => 'field_'.$block.'_2',
+		'label' => 'Testimonial',
+		'name' => 'testimonial',
+		'type' => 'textarea',
+		'column_width' => '',
+		'default_value' => '',
+		'placeholder' => '',
+		'maxlength' => '',
+		'rows' => '',
+		'formatting' => 'none',
+	),
+	array (
+		'key' => 'field_'.$block.'_3',
+		'label' => 'Image',
+		'name' => 'image',
+		'type' => 'image',
+		'instructions' => '(Optional)',
+		'column_width' => '',
+		'save_format' => 'object',
+		'preview_size' => 'thumbnail',
+		'library' => 'all',
+	),
+	array (
+		'key' => 'field_'.$block.'_4',
+		'label' => 'Attribution Title',
+		'name' => 'attribution',
+		'type' => 'text',
+		'instructions' => '(Optional)',
+		'column_width' => '',
+		'default_value' => '',
+		'placeholder' => '',
+		'prepend' => '',
+		'append' => '',
+		'formatting' => 'none',
+		'maxlength' => '',
+	),
+	array (
+		'key' => 'field_'.$block.'_attribution_sub_title',
+		'label' => 'Attribution Sub Title',
+		'name' => 'attribution_sub_title',
+		'type' => 'text',
+		'instructions' => '(Optional)',
+		'column_width' => '',
+		'default_value' => '',
+		'placeholder' => '',
+		'prepend' => '',
+		'append' => '',
+		'formatting' => 'none',
+		'maxlength' => '',
+	),
+);
+
+if ($testimonial_cpts) {
+	foreach ($block_sub_fields as $key => $field) {
+		$block_sub_fields[$key]['conditional_logic'] = array (
+		    'status' => 1,
+		    'rules' => array (
+		        array (
+		           'field' => 'field_'.$block.'_use_post',
+		            'operator' => '!=',
+		           	'value' => 1,
+		      	),
+		    ),
+		    'allorany' => 'all',
+		);
+	}
+	$block_sub_fields[] = array (
+	   'key' => 'field_'.$block.'_use_post',
+	   'label' => 'Use Post',
+	   'name' => 'use_post',
+	   'type' => 'true_false',
+	   'instructions' => '',
+	   'required' => 0,
+	   'conditional_logic' => 0,
+	   'wrapper' => array (
+	       'width' => '',
+	       'class' => '',
+	       'id' => '',
+	   ),
+	   'message' => '',
+	   'ui' => 1,
+	   'ui_on_text' => 'Yes',
+	   'ui_off_text' => 'No',
+	   'default_value' => 0,
+   );
+
+   $block_sub_fields[] = array (
+       'key' => 'field_'.$block.'_testimonial_post',
+       'label' => 'Testimonial Post',
+       'name' => 'testimonial_post',
+       'type' => 'post_object',
+       'instructions' => '',
+       'required' => 0,
+       'conditional_logic' => array (
+           'status' => 1,
+           'rules' => array (
+               array (
+                  'field' => 'field_'.$block.'_use_post',
+                   'operator' => '==',
+                  'value' => 1,
+             ),
+           ),
+           'allorany' => 'all',
+       ),
+       'wrapper' => array (
+           'width' => '',
+           'class' => '',
+           'id' => '',
+       ),
+       'post_type' => $testimonial_cpts,
+       'allow_null' => 0,
+       'multiple' => 0,
+       'return_format' => 'id',     // object | id
+       'ui' => 1,
+   );
+
+}
+
 $block_fields = array(
 	array (
 		'key' => 'field_'.$block.'_1',
@@ -19,59 +141,7 @@ $block_fields = array(
 		'name' => 'testimonials',
 		'type' => 'repeater',
 		'column_width' => '',
-		'sub_fields' => array (
-			array (
-				'key' => 'field_'.$block.'_2',
-				'label' => 'Testimonial',
-				'name' => 'testimonial',
-				'type' => 'textarea',
-				'column_width' => '',
-				'default_value' => '',
-				'placeholder' => '',
-				'maxlength' => '',
-				'rows' => '',
-				'formatting' => 'none',
-			),
-			array (
-				'key' => 'field_'.$block.'_3',
-				'label' => 'Image',
-				'name' => 'image',
-				'type' => 'image',
-				'instructions' => '(Optional)',
-				'column_width' => '',
-				'save_format' => 'object',
-				'preview_size' => 'thumbnail',
-				'library' => 'all',
-			),
-			array (
-				'key' => 'field_'.$block.'_4',
-				'label' => 'Attribution Title',
-				'name' => 'attribution',
-				'type' => 'text',
-				'instructions' => '(Optional)',
-				'column_width' => '',
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'none',
-				'maxlength' => '',
-			),
-			array (
-				'key' => 'field_'.$block.'_attribution_sub_title',
-				'label' => 'Attribution Sub Title',
-				'name' => 'attribution_sub_title',
-				'type' => 'text',
-				'instructions' => '(Optional)',
-				'column_width' => '',
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'none',
-				'maxlength' => '',
-			),
-		),
+		'sub_fields' => $block_sub_fields,
 		'min' => '1',
 		'max' => '',
 		'layout' => 'row',
