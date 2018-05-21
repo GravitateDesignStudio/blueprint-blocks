5b6bd10bd4c9f8d751f787783f5bef190fba454a
Scott Haselwood <scott@Scott-Haselwood.local>
2018-05-21 14:53:29 -0700

    Updated Accordion/Tabs

--- grav-blocks/accordion-tabs/block.php
+++ grav-blocks/accordion-tabs/block.php
@@ -2,7 +2,13 @@
 
 $format = isset($format) ? $format : get_sub_field('format');
 $sections = isset($sections) ? $sections : get_sub_field('sections');
+
 $add_side_content = isset($add_side_content) ? $add_side_content : get_sub_field('add_side_content');
+$side_content = isset($side_content) ? $side_content : get_sub_field('side_content');
+
+$accordion_col = ($add_side_content) ? 8 : 10;
+
+$accordion_col = ($add_side_content && $format != 'tabs-left') ? $accordion_col : 12;
 
 if ($add_side_content) {
 	$side_content_placement = get_sub_field('side_content_placement');
@@ -17,14 +23,12 @@ if ($add_side_content) {
 
 	<div class="block-inner">
 		<div class="<?php echo GRAV_BLOCKS::css()->row()->add('align-center')->get();?>">
-			<?php if ($add_side_content): ?>
-			<div class="<?php echo GRAV_BLOCKS::css()->col(12, 4, 5)->add(array('block-accordion-tabs__side-content', ($add_side_content) ? $content_classes : ''))->get(); ?>">
-				<?php if ($side_content = get_sub_field('side_content')) { ?>
-					<?php echo $side_content; ?>
-				<?php } ?>
+			<?php if ($add_side_content && $side_content): ?>
+			<div class="<?php echo GRAV_BLOCKS::css()->col(12, 4, 4)->add(array('block-accordion-tabs__side-content', ($add_side_content) ? $content_classes : ''))->get(); ?>">
+				<?php echo $side_content; ?>
 			</div>
 			<?php endif; ?>
-			<div class="<?php echo GRAV_BLOCKS::css()->col(12, ($add_side_content) ? 8 : 12, ($add_side_content) ? 5 : 10)->add(array('block-accordion-tabs__container',($add_side_content) ? $accordion_classes : '', $format))->get(); ?>">
+			<div class="<?php echo GRAV_BLOCKS::css()->col(12, $accordion_col)->add(array('block-accordion-tabs__container',($add_side_content) ? $accordion_classes : '', $format))->get(); ?>">
 				<?php if ($format == 'tabs-top' || $format == 'tabs-left') { ?>
 					<ul class="block-accordion-tabs__tab-list show-for-medium">
 						<?php foreach ($sections as $key => $section): ?>
