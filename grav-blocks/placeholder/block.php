fbd56cab614d4ea3a7bef63d1ba3c40327c9ed2b
Scott Haselwood <scott@Scott-Haselwood.local>
2018-05-22 15:42:18 -0700

    Updated columns for placeholder block

--- grav-blocks/placeholder/block.php
+++ grav-blocks/placeholder/block.php
@@ -3,9 +3,9 @@
 if($image = get_sub_field('image')){ ?>
 	<div class="block-inner">
 		<div class="<?php echo GRAV_BLOCKS::css()->row()->get(); ?>">
-			<?php if($padding){ ?><div class="columns small-12"><?php } ?>
-					<?php echo GRAV_BLOCKS::image($image);?>
-			<?php if($padding){ ?></div><?php } ?>
+			<div class="columns small-12">
+				<?php echo GRAV_BLOCKS::image($image);?>
+			</div>
 		</div>
 	</div>
 <?php } ?>
