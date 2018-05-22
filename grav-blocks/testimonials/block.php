3d8d19210d983a6b93c47e14b794dd812c217b81
Scott Haselwood <scott@Scott-Haselwood.local>
2018-05-22 11:30:16 -0700

    Updated filter functionality

--- grav-blocks/testimonials/block.php
+++ grav-blocks/testimonials/block.php
@@ -2,6 +2,11 @@
 
 $testimonials = isset($testimonials) ? $testimonials : get_sub_field('testimonials');
 
+if(apply_filters( 'grav_blocks_testimonials_cpt', $testimonial_cpts)) {
+	// TODO: Figure this out
+	$schema = apply_filters('grav_blocks_testimonials_cpt_schema', $testimonial_cpts_schema);
+}
+
 if($testimonials)
 {
 	?>
@@ -14,36 +19,44 @@ if($testimonials)
 						<?php
 						foreach ($testimonials as $testimonial) {
 
-							$image = $testimonial['image'];
-							$text = $testimonial['testimonial'];
-							$attribution = $testimonial['attribution'];
-							$attribution_sub_title = $testimonial['attribution_sub_title'];
-
 							if ($testimonial['use_post']) {
-								echo $testimonial['testimonial_post'];
-								$testimonial_post = get_post($testimonial['testimonial_post']);
+								global $post;
+								$post = get_post($testimonial['testimonial_post']);
+								setup_postdata( $post );
+								$post_type = get_post_type( $testimonial_post );
+								$image = wp_get_attachment_image(get_post_thumbnail_id());
+								$text = get_the_content();
+								$attribution = get_the_title();
+								$attribution_sub_title = get_field('attribution_sub_title');
+
+							} else {
 
 								$image = $testimonial['image'];
-								$text = $testimonial_post->post_content;
-								$attribution = $testimonial_post->post_title;
-								$attribution_sub_title = 'CPT';
-								// $text = $testimonial['testimonial'];
-								// $attribution = $testimonial['attribution'];
-								// $attribution_sub_title = $testimonial['attribution_sub_title'];
+								$text = $testimonial['testimonial'];
+								$attribution = $testimonial['attribution'];
+								$attribution_sub_title = $testimonial['attribution_sub_title'];
 
 							} ?>
 
 							<div class="block-testimonials__testimonial swiper-slide">
 								<div class="<?php echo GRAV_BLOCKS::css()->add(($image ? 'has-image' : 'no-image'))->row()->get();?>">
-									<?php if($image){?>
+									<?php if ($image_array):
+
+										var_dump($image_array);
+
+									endif; ?>
+
+									<?php if($image){ ?>
 									<div class="<?php echo GRAV_BLOCKS::css()->add('block-testimonials__testimonial--image')->col(12, 2)->get();?>">
-										<img src="<?php echo esc_attr($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+										<?php if (is_array($image)): ?>
+											<img src="<?php echo esc_attr($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+										<?php else: ?>
+											<?php echo $image; ?>
+										<?php endif; ?>
+
 									</div>
 									<?php } ?>
 									<div class="<?php echo GRAV_BLOCKS::css()->add('block-testimonials__testimonial--content')->col(12, ($image ? 10 : 12))->get();?>">
-										<?php if ($testimonial_post) {
-											var_dump($testimonial_post);
-										} ?>
 										<blockquote class="testimonial">
 											<p><?php echo $text; ?></p>
 											<footer>
@@ -56,7 +69,11 @@ if($testimonials)
 									</div>
 								</div>
 							</div>
-						<?php } ?>
+						<?php
+						if ($testimonial['use_post']) {
+							wp_reset_postdata();
+						}
+					 } ?>
 
 					</div>
 
