fbb0600a721faf2fbdb99919b407098415cb5078
Doug Frei <dougfrei@gmail.com>
2019-04-29 11:47:22 -0700

    title block updates

--- grav-blocks/title/block.php
+++ grav-blocks/title/block.php
@@ -1,31 +1,33 @@
 <?php
-
 $heading = isset($heading) ? $heading : get_sub_field('title');
-$sub_heading = isset($sub_heading) ? $sub_heading : get_sub_field('sub-title');
-$intro = isset($intro) ? $intro : get_sub_field('intro');
 
-$center = isset($center) ? $center : get_sub_field('center');
+if ($heading)
+{
+	$subheading = isset($subheading) ? $subheading : get_sub_field('sub-title');
+	$center = isset($center) ? $center : get_sub_field('center');
 
-if($heading){
-?>
+	$heading_tag = apply_filters('grav_blocks_title_heading_tag', 'h2');
+	$subheading_tag = apply_filters('grav_blocks_title_subheading_tag', 'h3');
+
+	?>
 	<div class="block-inner">
-		<div class="<?php echo GRAV_BLOCKS::css()->row()->get();?>">
-			<div class="<?php echo GRAV_BLOCKS::css()->col()->get();?>">
-				<h2 class="block-title__title"<?php if($center){?> style="text-align:center;"<?php } ?>>
-					<?php echo $heading; ?>
-				</h2>
-				<?php if($sub_heading){ ?>
-					<h3 class="block-title__sub-title"<?php if($center){?> style="text-align:center;"<?php } ?>>
-						<?php echo $sub_heading; ?>
-					</h3>
-				<?php } ?>
-				<?php if($intro){ ?>
-					<p class="block-title__intro"<?php if($center){?> style="text-align:center;"<?php } ?>>
-						<?php echo $intro; ?>
-					</p>
-				<?php } ?>
+		<div class="<?php echo GRAV_BLOCKS::css()->row()->get(); ?>">
+			<div class="<?php echo GRAV_BLOCKS::css()->col()->get(); ?>">
+				<<?php echo esc_attr($heading_tag); ?> class="block-title__title"<?php if ($center) { ?> style="text-align:center;"<?php } ?>>
+					<?php echo esc_html($heading); ?>
+				</<?php echo esc_attr($heading_tag); ?>>
+				<?php
+				if ($subheading)
+				{
+					?>
+					<<?php echo esc_attr($subheading_tag); ?> class="block-title__sub-title"<?php if ($center) { ?> style="text-align:center;"<?php } ?>>
+						<?php echo esc_html($subheading); ?>
+					</<?php echo esc_attr($subheading_tag); ?>>
+					<?php
+				}
+				?>
 			</div>
 		</div>
 	</div>
-<?php
+	<?php
 }
