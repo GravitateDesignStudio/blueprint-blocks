2159e157920335f4a8ee37beb2218d8f00c7de8a
Jeremy Ross <jeremy.ross@gravitatedesign.com>
2023-05-25 15:48:18 -0700

    minor class spacing

--- grav-blocks/title/block.php
+++ grav-blocks/title/block.php
@@ -4,18 +4,18 @@ $subheading = isset($subheading) ? esc_html($subheading) : esc_html(get_sub_fiel
 
 $heading_element = get_sub_field('heading_element') != 'h2' ? get_sub_field('heading_element') : '';
 $subheading_element = get_sub_field('subheading_element') != 'h3' ? get_sub_field('subheading_element') : '';
-$center = get_sub_field('center') ? 'text-center' : '';
+$center = get_sub_field('center') ? 'text-center ' : '';
 
 if (!$heading) return;
 ?>
 
 <div class="block-inner row">
 	<div class="column">
-		<h2 class="<?= $center ?> <?= $heading_element ?>">
+		<h2 class="<?= $center ?><?= $heading_element ?>">
 			<?= $heading ?>
 		</h2>
 		<?php if ($subheading) : ?>
-			<h3 class="subheading <?= $center ?> <?= $subheading_element ?>">
+			<h3 class="subheading <?= $center ?><?= $subheading_element ?>">
 				<?= $subheading ?>
 			</h3>
 		<?php endif ?>
